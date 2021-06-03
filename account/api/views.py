0faf3ddeb8367dfda7893d765f1b367a16e6b9a5
Həsən Tağıyev <hasan.tagiyev@mdm.gov.az>
2021-06-03 18:05:47 +0400

    login logout with token

---
+++ account/api/views.py
@@ -0,0 +1,35 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from account.api.serializers import RegistrationSerializer
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@api_view(['POST', ])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = 'successfully registered new user.'
+            data['email'] = account.email
+            data['username'] = account.username
+        else:
+            data = serializer.errors
+        return Response(data)
+
+
+@api_view(['GET', ])
+@permission_classes((IsAuthenticated, ))
+def logout(request):
+    user = request.user
+    if user:
+        user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
